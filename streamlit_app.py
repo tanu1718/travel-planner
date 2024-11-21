565f60dde67782a6eb72a1a531d588bdf3e9bca4
tanu1718 <trana01@g.syr.edu>
2024-11-21 16:20:20 +0000

    chg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 from auth import login_page, get_user
 
 # Set up a sidebar or navigation for different pages
@@ -16,27 +16,27 @@ user_email = get_user()
 st.sidebar.title(f"Welcome, {user_email.split('@')[0]}")
 
 # Define navigation using a simple option menu
-with st.sidebar:
-    selected_page = option_menu(
-        "Select Lab",
-        ["Manual & AI Assisted Editting", "Second Lab", "Third Lab", "Fourth Lab", "Fifth Lab"],
-        icons=['book', 'book', 'book', 'book', 'book'],
-        menu_icon="cast", 
-        default_index=0,
-    )
+# with st.sidebar:
+#     selected_page = option_menu(
+#         "Select Lab",
+#         ["Manual & AI Assisted Editting", "Second Lab", "Third Lab", "Fourth Lab", "Fifth Lab"],
+#         icons=['book', 'book', 'book', 'book', 'book'],
+#         menu_icon="cast", 
+#         default_index=0,
+#     )
 
 # Load the appropriate page based on the user's selection
-if selected_page == "Manual & AI Assisted Editting":
-    st.title("First Lab")
-    # Execute the page1.py code
-    exec(open("page1.py").read())  # This will run the content of page1.py
+# if selected_page == "Manual & AI Assisted Editting":
+#     st.title("First Lab")
+#     # Execute the page1.py code
+#     exec(open("page1.py").read())  # This will run the content of page1.py
 
-elif selected_page == "Second Lab":
-    st.title("Second Lab")
-    # Execute the Lab2.py code
-    exec(open("Lab2.py").read())  # This will run the content of Lab2.py
+# elif selected_page == "Second Lab":
+#     st.title("Second Lab")
+#     # Execute the Lab2.py code
+#     exec(open("Lab2.py").read())  # This will run the content of Lab2.py
 
-elif selected_page == "Third Lab":
-    st.title("Third Lab")
-    # Execute the Lab3.py code
-    exec(open("Lab3.py").read())  # This will run the content of Lab3.py
+# elif selected_page == "Third Lab":
+#     st.title("Third Lab")
+#     # Execute the Lab3.py code
+#     exec(open("Lab3.py").read())  # This will run the content of Lab3.py
